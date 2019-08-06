7a14c3fe658ec8cc2ff5ce1dd613f2f6dd6303c8
Martin <martin.d@live.se>
2019-08-06 03:26:27 +0200

    added docker tests and ci configs

--- reset-test.bash
+++ reset-test.bash
@@ -1,6 +1,9 @@
+# cd ./src/db
+# $(> test.sqlite)
+# cat migrate.sql | sqlite3 test.copy.sqlite
+# cd ../..
+
+# Becuace docker cant find sqlite3 command
 cd ./src/db
-
-$(> test.sqlite)
-cat migrate.sql | sqlite3 test.sqlite
-
+cp test.copy.sqlite test.sqlite
 cd ../..
